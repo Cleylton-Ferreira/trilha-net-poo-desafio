4252707299f2ba64d55d32d65de1318238c5b3a8
LittleEnd <cleylton.ferreira@hotmail.com>
2024-10-30 23:48:32 -0300

    Finalizado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,21 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone()
+        {
+            
+        }      
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;            
         }
 
         public void Ligar()
@@ -19,6 +28,19 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+        protected void ExibirDadosSmartphone()
+        {
+            System.Console.WriteLine
+            (@$"
+            Dados do Smartphone
+            _____________________________________
+
+                Número de Telefone: {Numero}
+                Nome do Modelo: {Modelo}
+                Número do IMEI: {IMEI}
+                Quantidade de Memória: {Memoria}GB
+            ");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia= new Nokia("12345678","CB-300","11111111111", 64 );
+nokia.Ligar();
+nokia.InstalarAplicativo("Banco Caixa Econômica Federal");
+nokia.ExibirDadosSmartphoneNokia();
+
+System.Console.WriteLine("\n");
+
+Iphone iphone= new Iphone("87654321", "Fan-150", "12345678911", 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Banco do Brasil");
+iphone.ExibirDadosSmartphoneIphone();
